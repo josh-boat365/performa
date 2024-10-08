e609c3194c4e52eeeb1d99fa716e5f3cbb82e10b
josh-boat365 <nyarko.boateng20@outlook.com>
2024-10-08 14:31:11 +0000

    added setup pages for score, kpi and sections

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,4 +9,15 @@ class DashboardController extends Controller
     public function index(){
         return view("dashboard.index");
     }
+
+    public function kpi_setup(){
+        return view("dashboard.kpi-setup.kpi-setup");
+    }
+    public function score_setup(){
+        return view("dashboard.kpi-setup.score-setup");
+    }
+
+    public function section_setup(){
+        return view("dashboard.kpi-setup.section-setup");
+    }
 }
