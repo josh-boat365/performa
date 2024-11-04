20be9bfc5fca1f2aae8c07c59489c5dd7af7da31
josh-boat365 <nyarko.boateng20@outlook.com>
2024-11-04 14:49:50 +0000

    added bootstrap, js and css

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -8,15 +8,17 @@
     <meta content="A name that suggests performance evaluation and improvement." name="description" />
     <meta content="BPSL" name="author" />
     <!-- App favicon -->
-    <link rel="shortcut icon" href="{{ asset('bpsl_imgs/performa-short.jpg')}}">
+    <link rel="shortcut icon" href="{{ asset('bpsl_imgs/performa-short.jpg') }}">
 
     <!-- Bootstrap Css -->
-    <link href="{{ asset('/assets/css/bootstrap.min.css')}}"  rel="stylesheet" type="text/css" />
+    <link href="{{ asset('/assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
     <!-- Icons Css -->
-    <link href="{{ asset('/assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('/assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
     <!-- App Css-->
-    <link href="{{ asset('/assets/css/app.min.css')}}"  rel="stylesheet" type="text/css" />
+    <link href="{{ asset('/assets/css/app.min.css') }}" rel="stylesheet" type="text/css" />
     <!-- App js -->
-    <script src="{{ asset('/assets/js/plugin.js')}}"></script>
+    <script src="{{ asset('/assets/js/plugin.js') }}"></script>
+
+  
 
 </head>
