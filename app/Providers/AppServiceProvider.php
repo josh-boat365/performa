1a0d4462eea5f0d8cfe30bdafffb97dc063d177b
josh-boat365 <nyarko.boateng20@outlook.com>
2024-11-04 10:11:44 +0000

    added user access authorization for viewing logs

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
+use Opcodes\LogViewer\Facades\LogViewer;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +22,17 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrapFive();
+
+        LogViewer::auth(function ($request) {
+            return $request->user()
+                && in_array($request->user()->email, [
+                    'jnboateng@bestpointgh.com',
+                    'cashun@bestpointgh.com',
+                    'ranane@bestpointgh.com',
+                    'dkwarteng@bestpointgh.com',
+                    'ekwakye@bestpointgh.com',
+                    'kwame.kay365@gmail.com',
+                ]);
+        });
     }
 }
