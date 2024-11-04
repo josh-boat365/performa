98afec1aa34d66266b800eb30c0ab15dd7ba94f1
josh-boat365 <nyarko.boateng20@outlook.com>
2024-11-04 14:41:35 +0000

    UI updates on Login

--- resources/views/auth/auth-login.blade.php
+++ resources/views/auth/auth-login.blade.php
@@ -3,6 +3,15 @@
 
     <div class="container">
         <div class="row justify-content-center">
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
             <div class="col-md-8 col-lg-6 col-xl-5">
                 <div class="card overflow-hidden">
                     <div class="" style="background-color: #c655e640 !important">
@@ -32,41 +41,49 @@
 
                         </div>
                         <div class="p-2">
-                            <form class="form-horizontal" action="{{ route('dashboard.index') }}">
+                            <form class="form-horizontal" action="{{ route('login.post') }}" method="POST">
+                                @csrf
 
                                 <div class="mb-3">
                                     <label for="username" class="form-label">Username</label>
-                                    <input type="text" class="form-control" id="username"
-                                        placeholder="Enter username">
+                                    <input type="text" name="username"
+                                        class="form-control @error('username') is-invalid @enderror" id="username"
+                                        placeholder="Enter username" value="{{ old('username') }}">
+
+                                    @error('username')
+                                        <div class="invalid-feedback">{{ $message }}</div>
+                                    @enderror
                                 </div>
 
                                 <div class="mb-3">
                                     <label class="form-label">Password</label>
                                     <div class="input-group auth-pass-inputgroup">
-                                        <input type="password" class="form-control" placeholder="Enter password"
-                                            aria-label="Password" aria-describedby="password-addon">
-                                        <button class="btn btn-light " type="button" id="password-addon"><i
+                                        <input type="password" name="password"
+                                            class="form-control @error('password') is-invalid @enderror"
+                                            placeholder="Enter password" aria-label="Password"
+                                            aria-describedby="password-addon">
+                                        <button class="btn btn-light" type="button" id="password-addon"><i
                                                 class="mdi mdi-eye-outline"></i></button>
+
+                                        @error('password')
+                                            <div class="invalid-feedback">{{ $message }}</div>
+                                        @enderror
                                     </div>
                                 </div>
 
                                 <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" id="remember-check">
-                                    <label class="form-check-label" for="remember-check">
-                                        Remember me
-                                    </label>
+                                    <input class="form-check-input" type="checkbox" id="remember-check" name="remember">
+                                    <label class="form-check-label" for="remember-check">Remember me</label>
                                 </div>
 
                                 <div class="mt-3 d-grid">
-                                    <button class="btn btn-primary waves-effect waves-light" type="submit">Log
+                                    <button type="submit" class="btn btn-primary waves-effect waves-light">Log
                                         In</button>
                                 </div>
 
-
-
                                 <div class="mt-4 text-center">
-                                    <a href="auth-recoverpw.html" class="text-muted"><i class="mdi mdi-lock me-1"></i>
-                                        Forgot your password?</a>
+                                    <a href="#" class="text-muted"><i class="mdi mdi-lock me-1"></i> Forgot your
+                                        password?</a>
                                 </div>
                             </form>
                         </div>
