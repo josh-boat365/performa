3d1d520fb7b9e085c781c358f3987bbaac83374d
josh-boat365 <nyarko.boateng20@outlook.com>
2024-11-20 10:23:41 +0000

    updated footer with sweet alert confirm delete

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -35,7 +35,36 @@
 
 <script src="{{ asset('assets/js/pages/rating-init.js') }}"></script>
 
+<!-- Sweet Alerts js -->
+<script src="{{ asset('assets/libs/sweetalert2/sweetalert2.min.js') }}"></script>
+
+<!-- Sweet alert init js-->
+<script src="{{ asset('assets/js/pages/sweet-alerts.init.js') }}"></script>
+
 
 
 <!-- App js -->
 <script src="{{ asset('assets/js/app.js') }}"></script>
+
+
+@push('scripts')
+    <script>
+        // SWEETALERT CONFIRM DELETE
+        function confirmDelete(button) {
+            Swal.fire({
+                title: 'Are you sure?',
+                text: "You won't be able to revert this!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, delete it!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    // Get the form that contains the button clicked
+                    button.closest('form').submit();
+                }
+            });
+        }
+    </script>
+@endpush
