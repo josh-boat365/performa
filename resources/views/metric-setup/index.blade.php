9ac2578c52cc08f834d6ce05da65ab4500bb80f4
josh-boat365 <nyarko.boateng20@outlook.com>
2024-11-22 07:08:18 +0000

    Bug fix for metric creation #7

--- resources/views/metric-setup/index.blade.php
+++ resources/views/metric-setup/index.blade.php
@@ -7,13 +7,20 @@
         <div class="row">
             <div class="col-12">
                 <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                    <h4 class="mb-sm-0 font-size-18"><a href="{{ route('kpi.index') }}">Setup For Sections</a> > Metric
-                        Setup For > <a href="#">{{ session('section_metric_name') }}</a>
+                    <h4 class="mb-sm-0 font-size-18"><a href="#">Setup For Section</a> > Metrics
+                        {{--  > <a href="#">{{ session('section_metric_name') }}</a>  --}}
                     </h4>
                 </div>
             </div>
         </div>
         <!-- end page title -->
+
+        <div style="">
+
+            <button type="button" class="btn btn-success btn-rounded waves-effect waves-light " data-bs-toggle="offcanvas"
+                data-bs-target="#offcanvasRight" aria-controls="offcanvasRight"><i class="bx bxs-plus"></i>Create
+                Metric for Section</button>
+        </div>
 
         <div class="mt-4 mb-4" style="background-color: gray; height: 1px;"></div>
 
@@ -34,11 +41,11 @@
                                 </div>
                             </div>
                             <div class="col-sm-4">
-                                <div class="text-sm-end">
+                                {{--  <div class="text-sm-end">
                                     <button type="button" data-bs-toggle="modal" data-bs-target=".bs-example-modal-xl"
                                         class="btn btn-success btn-rounded waves-effect waves-light mb-2 me-2 "><i
                                             class="mdi mdi-plus me-1"></i> Create Metric for Section</button>
-                                </div>
+                                </div>  --}}
                             </div><!-- end col-->
                         </div>
 
@@ -49,32 +56,36 @@
                                 <thead class="table-light">
                                     <tr>
 
+                                        <th class="align-middle">Section Metric </th>
                                         <th class="align-middle">Metric Name</th>
                                         <th class="align-middle">Metric Score</th>
                                         <th class="align-middle">Metric Description</th>
+                                        <th class="align-middle">Created At</th>
                                         <th class="align-middle">Action</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    @forelse ($filteredMetrics as $metric)
+                                    @forelse ($activeMetrics as $metric)
                                         <tr>
-                                            <td><a href="#">{{ $metric['name'] }}</a></td>
-                                            <td>{{ $metric['score'] }}</td>
-                                            <td>{{ $metric['description'] }}</td>
+                                            <td><a href="#">{{ $metric->section->name }}</a></td>
+                                            <td><a href="#">{{ $metric->name }}</a></td>
+                                            <td>{{ $metric->score }}</td>
+                                            <td>{{ $metric->description }}</td>
+                                            <td>{{ Carbon\Carbon::parse($metric->createdAt)->format('jS F, Y : g:i A') }}</td>
 
                                             <td>
-                                                <a href="{{ route('show.metric', $metric['id']) }}">
+                                                <a href="{{ route('show.metric', $metric->id) }}">
                                                     <span class="badge rounded-pill bg-primary fonte-size-13"><i
                                                             class="bx bxs-pencil"></i>edit</span>
                                                 </a>
                                                 <a href="#" data-bs-toggle="modal"
-                                                    data-bs-target=".bs-delete-modal-lg-{{ $metric['id'] }}">
+                                                    data-bs-target=".bs-delete-modal-lg-{{ $metric->id }}">
                                                     <span class="badge rounded-pill bg-danger fonte-size-13"><i
                                                             class="bx bxs-trash"></i> delete</span>
                                                 </a>
 
                                                 <!-- Modal for Delete Confirmation -->
-                                                <div class="modal fade bs-delete-modal-lg-{{ $metric['id'] }}"
+                                                <div class="modal fade bs-delete-modal-lg-{{ $metric->id }}"
                                                     tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel"
                                                     aria-hidden="true">
                                                     <div class="modal-dialog modal-md modal-dialog-centered">
@@ -90,8 +101,7 @@
                                                                 <h4 class="text-center mb-4">Are you sure you want to
                                                                     delete this
                                                                     Metric?</h4>
-                                                                <form
-                                                                    action="{{ route('delete.metric', $metric['id']) }}"
+                                                                <form action="{{ route('delete.metric', $metric->id) }}"
                                                                     method="POST">
                                                                     @csrf
                                                                     @method('DELETE')
@@ -123,52 +133,68 @@
                 </div>
 
                 <!--  Large modal example -->
-                <div class="modal fade bs-example-modal-xl" data-bs-backdrop="static" data-bs-keyboard="false"
-                    tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-                    <div class="modal-dialog modal-xl ">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="myLargeModalLabel">Create Metric For Section</h5>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                    aria-label="Close"></button>
+                <!-- right offcanvas -->
+                <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight"
+                    aria-labelledby="offcanvasRightLabel">
+                    <div class="offcanvas-header">
+                        <h5 id="offcanvasRightLabel">Setup For Metric</h5>
+                        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas"
+                            aria-label="Close"></button>
+                    </div>
+                    <div class="offcanvas-body">
+                        <form action="{{ route('create.metric') }}" class="custom-validation" method="POST">
+                            @csrf
+                            <div class="row mb-3">
+                                <label for="example-text-input" class="">Metric Name</label>
+                                <div class="col-md-12">
+                                    <input class="form-control" type="text" name="name" required
+                                        placeholder="Enter Name for Metric" value="{{ old('name') }}"
+                                        id="example-text-input">
+                                </div>
                             </div>
-                            <div class="modal-body">
 
-                                <form action="{{ route('create.metric') }}" method="POST">
-                                    @csrf
-
-                                    <div class="mb-3 col-lg-10">
-                                        <label for="email">Metric Name
-                                        </label>
-                                        <input type="text" name="name" class="form-control"
-                                            placeholder="Enter Metric Name" />
-                                    </div>
-                                    <div class="mb-3 col-sm-6">
-                                        <label for="name">Score</label>
-                                        <input type="number" id="score" name="score" class="form-control"
-                                            placeholder="Metric Score" />
-                                    </div>
-                                    <div class="mb-3 col-lg-10">
-                                        <label for="description">Metric
-                                            Description</label>
-                                        <input type="text" id="description" name="description"
-                                            class="form-control" placeholder="Enter Metric Description" />
-                                    </div>
-
-                                    <input type="hidden" name="active" value="1">
-                                    <input type="hidden" name="sectionId" value="{{ (session('section_metric_id')) }}">
-
-
-                                    <div class="d-grid mb-2">
-                                        <button type="submit" class="btn btn-success">Create
-                                            Metric</button>
-                                    </div>
-                                </form>
-
+                            <div class="row mb-3">
+                                <label for="example-text-input" class="">Metric Description</label>
+                                <div class="col-md-12">
+                                    <input class="form-control" type="text" name="description" required
+                                        placeholder="Enter Description for Metric" value="{{ old('description') }}"
+                                        id="example-text-input">
+                                </div>
                             </div>
-                        </div>
+
+                            <div class="row mb-3">
+                                <label for="example-text-input" class="">Metric Score</label>
+                                <div class="col-md-12">
+                                    <input class="form-control" type="number" name="score" required
+                                        value="{{ old('score') }}" id="example-text-input">
+                                </div>
+                            </div>
+
+                            <div class="row mb-3">
+                                <label for="example-text-input" class="">Select Section</label>
+                                <div class="col-md-12">
+                                    <select name="sectionId" class="form-select">
+                                        <option>Select Section</option>
+
+                                        @foreach ($sections as $section)
+                                            <option value="{{ $section->id }}"
+                                                {{ old('sectionId') == $section->id ? 'selected' : '' }}>
+                                                {{ $section->name }} - {{ $section->kpi->name }}
+                                            </option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+
+                            <input type="hidden" name="active" value="1">
+
+                            <button type="submit" class="btn btn-primary waves-effect waves-light col-md-12 mt-4">
+                                Create
+                            </button>
+                        </form>
                     </div>
                 </div>
+
             </div>
             <!-- end card -->
         </div>
