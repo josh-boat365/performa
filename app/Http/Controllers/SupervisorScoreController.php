90c955989add2006a54c5826230c0803d68e49a6
josh-boat365 <nyarko.boateng20@outlook.com>
2025-10-17 10:18:44 +0000

    Remove unused methods and comment validation checks
    Deleted commented-out edit methods and unused update/destroy methods from SupervisorScoreController. Also removed empty supervisor/probe comment validation checks in store and probScore methods to streamline form handling.

--- app/Http/Controllers/SupervisorScoreController.php
+++ app/Http/Controllers/SupervisorScoreController.php
@@ -44,180 +44,6 @@ class SupervisorScoreController extends Controller
     }
 
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    // public function edit(Request $request, $kpiId, $batchId)
-    // {
-
-    //     // Validate session
-    //     $sessionValidation = ValidateSessionController::validateSession();
-    //     if ($sessionValidation) {
-    //         return $sessionValidation;
-    //     }
-
-    //     // Get the access token from the session
-    //     $accessToken = session('api_token');
-
-    //     $data = [
-    //         'kpiId' => (int) $kpiId,
-    //         'batchId' => (int) $batchId
-    //     ];
-
-
-    //     try {
-
-    //         $response = Http::withToken($accessToken)
-    //             ->put("http://192.168.1.200:5123/Appraisal/Kpi/GetSupervisorScoringKpi", $data);
-
-    //         // dd($response);
-
-    //         if ($response->successful()) {
-
-    //             $kpis = $response->object();
-
-
-    //             $appraisal = collect();
-
-    //             // Process each KPI
-    //             foreach ($kpis as $kpi) {
-    //                 if ($kpi->kpiActive) {
-    //                     // Filter active sections
-    //                     $activeSections = collect($kpi->sections)->filter(function ($section) {
-    //                         return $section->sectionActive;
-    //                     });
-
-    //                     // Transform sections to include metrics, even if none are active
-    //                     $activeSections->transform(function ($section) {
-    //                         // Filter metrics within the section
-    //                         $section->metrics = collect($section->metrics)->filter(function ($metric) {
-    //                             return $metric->metricActive;
-    //                         });
-    //                         // Return the section regardless of whether it has active metrics
-    //                         return $section;
-    //                     });
-
-    //                     // Add the KPI and its sections to the appraisal
-    //                     $appraisal->push((object) [
-    //                         'kpi' => $kpi,
-    //                         'activeSections' => $activeSections
-    //                     ]);
-
-    //                     // Get the status of the first section safely
-    //                     if (!empty($kpi->sections)) {
-    //                         $firstSection = $kpi->sections[0];
-    //                         $status = $firstSection->sectionEmpScore->status ?? 'PENDING';
-    //                         $kpiStatus = $status;
-    //                     } else {
-    //                         $kpiStatus = 'PENDING'; // Handle the case where there are no sections
-    //                     }
-    //                 }
-    //             }
-
-
-
-
-
-    //             return view("dashboard.supervisor.score-employee-form", compact('appraisal', 'kpiStatus'));
-    //         } else {
-    //             // Log the error response
-    //             Log::error('Failed to retrieve KPIs', [
-    //                 'status' => $response->status(),
-    //                 'response' => $response->body()
-    //             ]);
-    //             return redirect()->back()->with('toast_error', 'Sorry, failed to retrieve Employee Appraisal, <b>Contact Application Support for Assistance</b>');
-    //         }
-    //     } catch (\Exception $e) {
-
-    //         Log::error('Exception occurred while retrieving KPIs', [
-    //             'message' => $e->getMessage(),
-    //             'trace' => $e->getTraceAsString()
-    //         ]);
-    //         return redirect()->back()->with('toast_error', 'Something went wrong, check your internet and try again, <b>Or Contact Application Support</b>');
-    //     }
-    // }
-
-
-
-    // public function editProb(Request $request, $kpiId, $batchId)
-    // {
-    //     // Validate session
-    //     $sessionValidation = ValidateSessionController::validateSession();
-    //     if ($sessionValidation) {
-    //         return $sessionValidation;
-    //     }
-
-    //     // Get the access token from the session
-    //     $accessToken = session('api_token');
-
-    //     $data = [
-    //         'kpiId' => (int) $kpiId,
-    //         'batchId' => (int) $batchId
-    //     ];
-
-
-    //     try {
-
-    //         $response = Http::withToken($accessToken)
-    //             ->put("http://192.168.1.200:5123/Appraisal/Kpi/GetProbScoringKpi", $data);
-
-
-    //         if ($response->successful()) {
-
-    //             $kpis = $response->object();
-
-
-    //             $appraisal = collect();
-
-    //             // Process each KPI
-    //             foreach ($kpis as $kpi) {
-    //                 if ($kpi->kpiActive) {
-    //                     // Filter active sections
-    //                     $activeSections = collect($kpi->sections)->filter(function ($section) {
-    //                         return $section->sectionActive;
-    //                     });
-
-    //                     // Transform sections to include metrics, even if none are active
-    //                     $activeSections->transform(function ($section) {
-    //                         // Filter metrics within the section
-    //                         $section->metrics = collect($section->metrics)->filter(function ($metric) {
-    //                             return $metric->metricActive;
-    //                         });
-    //                         // Return the section regardless of whether it has active metrics
-    //                         return $section;
-    //                     });
-
-    //                     // Add the KPI and its sections to the appraisal
-    //                     $appraisal->push((object) [
-    //                         'kpi' => $kpi,
-    //                         'activeSections' => $activeSections
-    //                     ]);
-    //                 }
-    //             }
-
-    //             // dd($appraisal);
-
-
-
-    //             // Return the KPI names and section counts to the view
-    //             return view("dashboard.probe-supervisor.score-employee-form", compact('appraisal'));
-    //         } else {
-    //             // Log the error response
-    //             Log::error('Failed to retrieve KPIs', [
-    //                 'status' => $response->status(),
-    //                 'response' => $response->body()
-    //             ]);
-    //             return redirect()->back()->with('toast_error', 'Sorry, failed to retrieve KPIs');
-    //         }
-    //     } catch (\Exception $e) {
-    //         // Log the exception
-    //         Log::error('Exception occurred while retrieving KPIs', [
-    //             'message' => $e->getMessage(),
-    //             'trace' => $e->getTraceAsString()
-    //         ]);
-    //         return redirect()->back()->with('toast_error', 'Something went wrong, check your internet and try again, <b>Or Contact Application Support</b>');
-    //     }
-    // }
 
     public function edit(Request $request, $kpiId, $batchId, $employeeId)
     {
@@ -331,8 +157,6 @@ class SupervisorScoreController extends Controller
 
 
 
-
-
     /**
      * Store a newly created resource in storage.
      */
@@ -362,9 +186,6 @@ class SupervisorScoreController extends Controller
 
                 // Ensure employeeComment is not an empty string
                 $supervisorComment = $request->input('supervisorComment', '');
-                if (trim($supervisorComment) === '') {
-                    return back()->with('toast_error', 'Supervisor comment cannot be empty.');
-                }
 
                 // Prepare the payload for the API request
                 $payload = [
@@ -387,9 +208,7 @@ class SupervisorScoreController extends Controller
                 ]);
 
                 $supervisorComment = $request->input('supervisorComment', '');
-                if (trim($supervisorComment) === '') {
-                    return back()->with('toast_error', 'Supervisor comment cannot be empty.');
-                }
+
 
                 // Prepare the payload for the API request
                 $payload = [
@@ -490,9 +309,7 @@ class SupervisorScoreController extends Controller
                 ]);
 
                 $probComment = $request->input('probComment', '');
-                if (trim($probComment) === '') {
-                    return back()->with('toast_error', 'Probe Supervisor comment cannot be empty.');
-                }
+
 
                 // Prepare the payload for the API request
                 $payload = [
@@ -515,9 +332,7 @@ class SupervisorScoreController extends Controller
                 ]);
 
                 $probComment = $request->input('probComment', '');
-                if (trim($probComment) === '') {
-                    return back()->with('toast_error', 'Probe Supervisor comment cannot be empty.');
-                }
+
 
                 // Prepare the payload for the API request
                 $payload = [
@@ -562,20 +377,4 @@ class SupervisorScoreController extends Controller
     }
 
 
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
 }
